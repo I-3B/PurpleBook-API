2d175536a8688e6dd3e4efb427821353f0696d1e
I3B <islamnaasani@gmail.com>
2022-03-07 22:26:17 +0200

    fix authentication problem and add checkUser function

--- src/routes/indexRouter.ts
+++ src/routes/indexRouter.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from "express";
+import checkRouteForAuth from "../utils/checkRouteForAuth";
 import APIRouter from "./APIRouter";
 const router = express.Router();
 
-router.use("/api", APIRouter);
+router.use("/api", checkRouteForAuth, APIRouter);
 router.use("/", (req: Request, res: Response) => {
     return res.status(200).json({ home: "hello" });
 });
--- src/strategies/jwtStrategy.ts
+++ src/strategies/jwtStrategy.ts
@@ -1,20 +1,26 @@
 require("dotenv").config();
+import User from "../models/User";
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
-const User = require("../models/User");
-
-const opts = { jwtFromRequest: null, secretOrKey: "" };
+const opts = { jwtFromRequest: "", secretOrKey: "" };
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = process.env.SECRET || "";
-
-export default new JwtStrategy(
-    opts,
-    (jwt_payload: { email: any }, done: (arg0: null, arg1: boolean) => any) => {
-        User.findOne({ email: jwt_payload.email }).exec(
-            (err: any, user: boolean) => {
-                if (err) return done(null, false);
-                return done(null, user);
+opts.secretOrKey = process.env.SECRET || "SECRET";
+export default new JwtStrategy(opts, function (
+    jwt_payload: { email: String },
+    done: (arg0: null, arg1: boolean) => any
+) {
+    User.findOne(
+        { email: jwt_payload.email },
+        { _id: 1 },
+        (err: null, user: boolean) => {
+            if (err) {
+                return done(err, false);
             }
-        );
-    }
-);
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        }
+    );
+});
