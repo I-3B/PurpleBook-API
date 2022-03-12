22181aff423fa3a47cc4bc64379c2741bb1e7194
I3B <islamnaasani@gmail.com>
2022-03-12 23:44:24 +0200

    add authorize middleware for user route

--- src/routes/APIRouter.ts
+++ src/routes/APIRouter.ts
@@ -1,7 +1,8 @@
 import express from "express";
+import authorizeUserRoute from "../utils/authorizeUserRoute";
 import authRouter from "./authRouter";
 import userRouter from "./userRouter";
 const router = express.Router();
 router.use("/auth", authRouter);
-router.use("/users", userRouter);
+router.use("/users/:userId", authorizeUserRoute, userRouter);
 export default router;
