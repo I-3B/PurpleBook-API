e99f26d69757a44e20271411b75a6ec17e692f8f
Islam Naasani <islamnaasani@gmail.com>
2022-05-17 19:09:24 +0300

    add 15 comments for one post

--- populateDB.ts
+++ populateDB.ts
@@ -9,13 +9,16 @@ const populateDB = async () => {
     const zero = (await login("Zero", 200)).body;
     const one = (await login("One", 200)).body;
 
-    const zeroPost = (await addPostWithImage(zero.token, 70, "/images/post.jpg", 201)).body;
-    await Promise.all([
-        addLikeToPost(zero.token, zeroPost.postId, 200),
-        addLikeToPost(one.token, zeroPost.postId, 200),
-        addComment(zero.token, zeroPost.postId, 50, 201),
-        addComment(one.token, zeroPost.postId, 50, 201),
-    ]);
+    for (let i = 1; i <= 25; i++) {
+        const zeroPost = (await addPostWithImage(zero.token, i, "/images/post.jpg", 201)).body;
+        await Promise.all([
+            addLikeToPost(zero.token, zeroPost.postId, 200),
+            addLikeToPost(one.token, zeroPost.postId, 200),
+        ]);
+        for (let i = 1; i <= 15; i++) {
+            await addComment(zero.token, zeroPost.postId, i, 201);
+        }
+    }
     await addFriendRequest(zero.userId, one.token, 200).then(async () => {
         await acceptFriendRequest(zero.userId, zero.token, one.userId, 200);
     });
