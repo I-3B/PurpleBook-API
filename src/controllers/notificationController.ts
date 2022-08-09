2934cdfeb1e01edb43883cdaa5095107463578aa
Islam Naasani <islamnaasani@gmail.com>
2022-08-09 13:44:12 +0300

    add skip and limit for getNotifications

--- src/controllers/notificationController.ts
+++ src/controllers/notificationController.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from "express";
 import Notification from "../models/Notification";
+import parseQuery from "../utils/parseQuery";
 
 const notificationController = {
     getNotifications: async (req: Request, res: Response) => {
+        const { skip, limit } = req.query;
+        const { skipValue, limitValue } = parseQuery(limit as string, 15, skip as string);
         const notifications = await Notification.find(
             { userId: req.user.id },
-            { updatedAt: 0 }
-        ).sort({ createdAt: -1 });
+            { updatedAt: 0, userId: 0 }
+        )
+            .sort({ createdAt: -1 })
+            .skip(skipValue)
+            .limit(limitValue);
         return res.status(200).json({ notifications });
     },
     setNotificationsAsViewed: async (req: Request, res: Response) => {
