c3f86192f3c7fe97b0038aaa47d51fa1174bd3b5
Islam Naasani <islamnaasani@gmail.com>
2022-08-16 00:28:20 +0300

    preparing heroku

--- src/configs/mongoConfigDevelopment.ts
+++ src/configs/mongoConfigDevelopment.ts
@@ -2,21 +2,15 @@ require("dotenv").config();
 
 import { MongoMemoryReplSet } from "mongodb-memory-server-global";
 import mongoose from "mongoose";
-if (process.env.NODE_ENV === "prod") {
-    mongoose.connect(process.env.MONGO_DB || "");
-    const db = mongoose.connection;
-    db.on("error", console.error.bind(console, "mongo connection error"));
-} else if (process.env.NODE_ENV === "development") {
-    MongoMemoryReplSet.create().then((mongoServer) => {
-        const mongoUri = mongoServer.getUri();
-        mongoose.connect(mongoUri);
+MongoMemoryReplSet.create().then((mongoServer) => {
+    const mongoUri = mongoServer.getUri();
+    mongoose.connect(mongoUri);
 
-        mongoose.connection.on("error", (e: { message: { code: string } }) => {
-            if (e.message.code === "ETIMEDOUT") {
-                console.log(e);
-                mongoose.connect(mongoUri);
-            }
+    mongoose.connection.on("error", (e: { message: { code: string } }) => {
+        if (e.message.code === "ETIMEDOUT") {
             console.log(e);
-        });
+            mongoose.connect(mongoUri);
+        }
+        console.log(e);
     });
-}
+});
