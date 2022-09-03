e2e21c9dbcde35bead6edab40fbb937768a110b8
Islam Naasani <islamnaasani@gmail.com>
2022-09-03 19:35:08 +0300

    add bool flag

--- src/utils/addFriendRequests.ts
+++ src/utils/addFriendRequests.ts
@@ -3,22 +3,19 @@ import { addFriendRequest } from "../../tests/utils/user.utils";
 
 require("dotenv").config();
 export function addFriendRequests(id: string, name?: string) {
-    if (
-        process.env.NODE_ENV === "production" ||
-        !["Zero", "One", "FriendOne", "FriendTwo", "RecOne", "RecTwo", "RecThree"].includes(
-            name || ""
-        )
-    ) {
-        const password = process.env.ACC_PASSWORD || "";
-        const acc1 = process.env.ACC1 || "";
-        const acc2 = process.env.ACC2 || "";
-        const acc3 = process.env.ACC3 || "";
-        loginAndAddFriendRequest(acc1, id, password);
-        loginAndAddFriendRequest(acc2, id, password);
-        loginAndAddFriendRequest(acc3, id, password);
+    if (process.env.NODE_ENV === "production" && process.env.AUTO_FRIEND_REQUEST === "true") {
+        const password = process.env.ACC_PASSWORD;
+        const acc1 = process.env.ACC1;
+        const acc2 = process.env.ACC2;
+        const acc3 = process.env.ACC3;
+        if (password) {
+            if (acc1) loginAndAddFriendRequest(acc1, id, password);
+            if (acc2) loginAndAddFriendRequest(acc2, id, password);
+            if (acc3) loginAndAddFriendRequest(acc3, id, password);
+        }
     }
 }
 async function loginAndAddFriendRequest(accName: string, userId: string, password: string) {
-    const { token, id } = (await login(accName, 200, password)).body;
+    const { token } = (await login(accName, 200, password)).body;
     await addFriendRequest(userId, token, 200);
 }
